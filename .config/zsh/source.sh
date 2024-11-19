a7164bb635a6404c93e8514499a1f559f34df975
robfalken <hello@robfalken.co>
2024-11-19 11:14:14 +0100

    source 1Password plugins

--- .config/zsh/source.sh
+++ .config/zsh/source.sh
@@ -1,2 +1,3 @@
 source $BREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $BREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $XDG_CONFIG_HOME/op/plugins.sh
