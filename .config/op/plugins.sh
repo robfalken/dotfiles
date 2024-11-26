912b47faa2063be9480149f39ae13a409bfff07f
robfalken <hello@robfalken.co>
2024-11-26 10:13:08 +0100

    Remove 1Password plugin

--- .config/op/plugins.sh
+++
@@ -1,2 +0,0 @@
-export OP_PLUGIN_ALIASES_SOURCED=1
-alias cdk="op plugin run -- cdk"
--- .config/zsh/source.sh
+++ .config/zsh/source.sh
@@ -1,3 +1,2 @@
 source $BREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $BREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $XDG_CONFIG_HOME/op/plugins.sh
