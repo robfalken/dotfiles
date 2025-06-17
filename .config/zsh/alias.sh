b11642b267220e95085e86bf4c7f4ed1c65ed8ae
robfalken <hello@robfalken.co>
2025-06-17 13:10:16 +0200

    zed config

--- .config/zsh/alias.sh
+++ .config/zsh/alias.sh
@@ -15,3 +15,4 @@ alias glg="git log --graph --pretty=tformat:'%Cred%h%Creset -%C(yellow)%d%Creset
 alias gcamend="git commit --amend --reuse-message=HEAD"
 alias reload-zshrc="source ~/.zshrc"
 alias ".."="cd .."
+alias opstaging="op run --no-masking --env-file .env.staging --"
